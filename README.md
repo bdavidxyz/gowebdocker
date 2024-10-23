c089fedf4e1d706ed0d4fb4a4bbfefb6d4dc60d1
David Boureau <bdavidxyz@gmail.com>
2024-10-23 15:19:38 +0200

    feat: working stuff

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simplest go web server
+
+Check go is installed on your machine with `go version`
+
+go run app.go
+
---
+++ app.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "fmt"      // formatting and printing values to the console.
+    "log"      // logging messages to the console.
+    "net/http" // Used for build HTTP servers and clients.
+)
+
+// Port we listen on.
+const portNum string = ":8080"
+
+// Handler functions.
+func Home(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "<html><body><h1> Hello, world! </h1><a href='/info'>info</a></body></html>")
+}
+
+func Info(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "<html><body><h1>Info</h1><a href='/'>back home</a></body></html>")
+}
+
+func main() {
+    log.Println("Starting our simple http server.")
+
+    // Registering our handler functions, and creating paths.
+    http.HandleFunc("/", Home)
+    http.HandleFunc("/info", Info)
+
+    log.Println("Started on port", portNum)
+    fmt.Println("To close connection CTRL+C :-)")
+
+    // Spinning up the server.
+    err := http.ListenAndServe(portNum, nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
